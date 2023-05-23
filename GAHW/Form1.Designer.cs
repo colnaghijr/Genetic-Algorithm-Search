39434d7d3941721819d226a7dd47bb94bff5e723
colnaghi <colnaghijr@gmail.com>
2023-05-23 13:48:15 -0700

    Add project files.

---
+++ GAHW/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GAHW
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtLookFor = new System.Windows.Forms.TextBox();
+            this.lblLookFor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(12, 44);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(373, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 73);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(373, 188);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // txtLookFor
+            // 
+            this.txtLookFor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLookFor.Location = new System.Drawing.Point(70, 12);
+            this.txtLookFor.Name = "txtLookFor";
+            this.txtLookFor.Size = new System.Drawing.Size(315, 20);
+            this.txtLookFor.TabIndex = 2;
+            this.txtLookFor.Text = "Hello World";
+            // 
+            // lblLookFor
+            // 
+            this.lblLookFor.AutoSize = true;
+            this.lblLookFor.Location = new System.Drawing.Point(12, 15);
+            this.lblLookFor.Name = "lblLookFor";
+            this.lblLookFor.Size = new System.Drawing.Size(52, 13);
+            this.lblLookFor.TabIndex = 3;
+            this.lblLookFor.Text = "Look For:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 273);
+            this.Controls.Add(this.lblLookFor);
+            this.Controls.Add(this.txtLookFor);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnStart);
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "frmMain";
+            this.Text = "Genetic Algorithm Search";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtLookFor;
+        private System.Windows.Forms.Label lblLookFor;
+    }
+}
+
---
+++ GAHW/GA.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GAHW
+{
+    public class CompareByFitness : IComparer<Citizen>
+    {
+        // Implement the IComparable interface. 
+        public int Compare(Citizen obj1, Citizen obj2)
+        {
+            return obj1.Fitness.CompareTo(obj2.Fitness);
+        }
+    } 
+
+    public class Citizen
+    {
+        public char[] Value;
+        public int Fitness;
+
+        public Citizen(int size)
+        {
+            Value = new char[size];
+            Fitness = 0;
+        }
+
+        public void RandomValue(Random r)
+        {
+            for (int j = 0; j < Value.Length; j++)
+            {
+                Value[j] = (char)((126 * r.NextDouble()) + 32);
+            }
+        }
+
+        public void CalculateFitness(char[] target)
+        {
+            int fitness = 0;
+            for (int j = 0; j < Value.Length; j++)
+            {
+                fitness += Math.Abs(Value[j] - target[j]);
+            }
+            Fitness = fitness;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("f:{1} v:{0}", new String(Value), Fitness);
+        }
+    }
+
+    public class GASearchResult
+    {
+        public Citizen Best;
+        public int GenerationsRun;
+    }
+
+    public delegate void SearchResultHandler(GASearchResult result);
+
+    public class GA
+    {
+        public event SearchResultHandler BestOfGenerationFound;
+        public event SearchResultHandler Finished;
+        public List<Citizen> Population;
+        public List<Citizen> NextGeneration;
+        public float elitismRate = 0.10f;
+        public float mutationRate = 0.25f;
+        public string targetWord;
+        public int maxGenerations;
+        public int maxPopulationPerGeneration;
+
+        public void StartGeneration()
+        { 
+            //Final string to find
+            char[] target = targetWord.ToCharArray();
+            //Size of string to easy search process
+            int targetSize = target.Length;
+            Population = new List<Citizen>(maxPopulationPerGeneration);
+            NextGeneration = new List<Citizen>(maxPopulationPerGeneration);
+
+            //Generate Initial population
+            GenerateInitialPopulation(targetSize, maxPopulationPerGeneration);
+            Citizen best = null;
+            int gen = 0;
+            for (; gen < maxGenerations; gen++)
+            {
+                //Start processing generation
+
+                //Fitness calculation
+                CalculateFitness(target);
+                //Print best so far
+                best = GetBest();
+
+                //Return best match so far
+                if (BestOfGenerationFound != null)
+                {
+                    GASearchResult result = new GASearchResult();
+                    result.Best = best;
+                    result.GenerationsRun = gen;
+                    BestOfGenerationFound(result);
+                }
+
+                //Get best & check
+                //If equal, done (optimal)
+                if (best.Fitness == 0) break;
+
+                //Mate
+                MatePopulation(targetSize, maxPopulationPerGeneration, elitismRate, mutationRate);
+
+                //Swap
+                SwapPopulation();
+            }
+
+            //Return best match
+            if (Finished != null)
+            {
+                GASearchResult result = new GASearchResult();
+                result.Best = best;
+                result.GenerationsRun = gen;
+                Finished(result);
+            }
+        }
+
+        private void GenerateInitialPopulation(int targetSize, int maxPopulationPerGeneration)
+        {
+            Random r = new Random();
+            for (int i = 0; i < maxPopulationPerGeneration; i++)
+            {
+                Citizen c = new Citizen(targetSize);
+                c.RandomValue(r);
+                Population.Add(c);
+            }
+        }
+
+        private void CalculateFitness(char[] target)
+        {
+            foreach (Citizen c in Population)
+            {
+                c.CalculateFitness(target);
+            }
+        }
+
+        private Citizen GetBest()
+        {
+            Citizen best = null;
+            foreach (Citizen c in Population)
+            {
+                if (best == null)
+                {
+                    best = c;
+                    continue;
+                }
+                if (c.Fitness < best.Fitness) best = c;
+            }
+            return best;
+        }
+
+        private void MatePopulation(int targetSize, int maxPopulationPerGeneration, float elitismRate, float mutationRate)
+        {
+            //Elitism
+            //Mutation
+            //Crossover
+            int elitSize = (int)(maxPopulationPerGeneration * elitismRate);
+            Random r = new Random();
+
+            Elitism(elitSize);
+
+            // Mate the rest
+            for (int i = elitSize; i < maxPopulationPerGeneration; i++)
+            {
+                NextGeneration.Add(new Citizen(targetSize));
+
+                int i1 = (int)(r.NextDouble() * maxPopulationPerGeneration);
+                int i2 = (int)(r.NextDouble() * maxPopulationPerGeneration);
+                int spos = (int)(r.NextDouble() * targetSize);
+
+                NextGeneration[i].Value =
+                    (new String(Population[i1].Value).Substring(0, spos) +
+                    new String(Population[i2].Value).Substring(spos, targetSize - spos)).ToCharArray();
+
+                if (r.NextDouble() < mutationRate) Mutate(NextGeneration[i], targetSize, r);
+            }
+        }
+
+        private void Mutate(Citizen citizen, int targetSize, Random r)
+        {
+            int ipos = (int)(r.NextDouble() * targetSize);
+            int mutantGene = (int)(r.NextDouble() * 126) + 32;
+
+            citizen.Value[ipos] = (char)mutantGene;
+        }
+
+        private void Elitism(int elitSize)
+        {
+            Population.Sort(new CompareByFitness());
+            for (int i = 0; i < elitSize; i++)
+            {
+                NextGeneration.Add(Population[i]);
+            }
+        }
+
+        private void SwapPopulation()
+        {
+            Population.Clear();
+            foreach (Citizen c in NextGeneration)
+            {
+                Population.Add(c);
+            }
+            NextGeneration.Clear();
+        }
+    }
+}
